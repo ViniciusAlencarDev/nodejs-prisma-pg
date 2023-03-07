3b5a21f79b5f0eb08a40b841cecfadb3e6751041
Vinicius Alencar <viniciusalencardev@gmail.com>
2023-03-07 08:54:34 -0300

    Post create user

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,18 @@ app.get('/', async () => {
     return await prisma.user.findMany();
 })
 
+app.post('/', async (request) => {
+
+    const { email, name } = request.body;
+
+    await prisma.user.create({
+        data: {
+            email,
+            name
+        }
+    });
+})
+
 const port = process.env.PORT ? Number(process.env.PORT) : 3333;
 
 app.listen({
