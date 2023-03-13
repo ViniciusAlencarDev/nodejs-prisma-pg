8aa7b4572de366a3d4d4352d11ab1b308833eb6b
Vinicius Alencar <viniciusalencardev@gmail.com>
2023-03-12 22:47:54 -0300

    Init test api

--- tests/index.ts
+++ tests/index.ts
@@ -1,5 +1,10 @@
+import Server from '../src/server';
+import supertest from 'supertest';
+
 describe('global', () => {
     test('API users', () => {
-        
+        const app = supertest(new Server(3001).start())
+
+        expect(10).toBe(10)
     })
 })
